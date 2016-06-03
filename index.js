ad17b5cade4467e171cf9608ef293e1f40aaecd4
Nitin Bansal <nitinbansal1989@yahoo.co.in>
2016-06-03 22:19:33 +0530

    Adding cache class

---
+++ index.js
@@ -0,0 +1,89 @@
+/// <reference path="./typings/globals/node/index.d.ts" />
+"use strict";
+class StoreValue {
+  constructor() {}
+  refresh() {
+    if (!isNaN(this.expire)) {
+      let x = this;
+      this.timeout = setTimeout(function() {
+        x.value = null;
+        if (x.valueFunc) {
+          x.value = x.valueFunc(x.key);
+          x.refresh();
+        }
+      }, this.expire);
+    }
+  }
+}
+class Cache {
+  constructor() {
+    this._store = new Map();
+  }
+  get(key) {
+    let s = this._store.get(key.toString());
+    if (s) {
+      return s.value;
+    }
+    return null;
+  }
+  put(key, val, expire, timeoutCallback) {
+    if (typeof expire !== 'undefined' && (typeof expire !== 'number' || isNaN(expire) || expire <= 0)) {
+      throw new Error("timeout is not a number or less then 0");
+    }
+    if (typeof timeoutCallback !== 'undefined' && typeof timeoutCallback !== 'function') {
+      throw new Error('Cache timeout callback must be a function');
+    }
+    let rec = new StoreValue();
+    rec.key = key;
+    rec.expire = expire;
+    if (val && typeof val === 'function') {
+      rec.valueFunc = val;
+      rec.value = rec.valueFunc(rec.key);
+    } else {
+      rec.value = val;
+    }
+    if (!isNaN(rec.expire)) {
+      let x = this;
+      if (rec.valueFunc) {
+        rec.refresh();
+      } else {
+        rec.timeout = setTimeout(function() {
+          x.del(rec.key);
+          if (timeoutCallback) {
+            timeoutCallback(key);
+          }
+        }, rec.expire);
+      }
+    }
+    this._store.set(key.toString(), rec);
+  }
+  del(key) {
+    let rec = this._store.get(key.toString());
+    if (rec) {
+      clearTimeout(rec.timeout);
+      this._store.delete(key.toString());
+      return true;
+    }
+    return false;
+  }
+  clear() {
+    for (let val of this._store.values()) {
+      clearTimeout(val.timeout);
+    }
+    this._store.clear();
+  }
+  size() {
+    return this._store.size;
+  }
+  keys() {
+    let res = new Array();
+    for (let key of this._store.keys()) {
+      res.push(key);
+    }
+    return res;
+  }
+}
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Cache;
---
+++ index.ts
@@ -0,0 +1,109 @@
+/// <reference path="./typings/globals/node/index.d.ts" />
+
+interface StoreCallback {
+  (key ? : (string | number | symbol)): any;
+}
+
+class StoreValue {
+  key: any;
+  value: any;
+  valueFunc: StoreCallback;
+  expire: number;
+  timeout: NodeJS.Timer;
+
+  constructor() {}
+
+  refresh() {
+    if (!isNaN(this.expire)) {
+      let x = this;
+      this.timeout = setTimeout(function() {
+        x.value = null;
+        if (x.valueFunc) {
+          x.value = x.valueFunc(x.key);
+          x.refresh();
+        }
+      }, this.expire);
+    }
+  }
+
+}
+
+class Cache {
+  _store: Map < String, StoreValue > = new Map < String, StoreValue > ();
+
+  constructor() {}
+
+  get(key: (string | number | symbol)): any {
+    let s = this._store.get(key.toString());
+    if (s) {
+      return s.value;
+    }
+    return null;
+  }
+
+  put(key: (string | number | symbol), val: any, expire ? : number, timeoutCallback ? : StoreCallback): void {
+    if (typeof expire !== 'undefined' && (typeof expire !== 'number' || isNaN(expire) || expire <= 0)) {
+      throw new Error("timeout is not a number or less then 0");
+    }
+
+    if (typeof timeoutCallback !== 'undefined' && typeof timeoutCallback !== 'function') {
+      throw new Error('Cache timeout callback must be a function');
+    }
+
+    let rec: StoreValue = new StoreValue();
+    rec.key = key;
+    rec.expire = expire;
+    if (val && typeof val === 'function') {
+      rec.valueFunc = val;
+      rec.value = rec.valueFunc(rec.key);
+    } else {
+      rec.value = val;
+    }
+    if (!isNaN(rec.expire)) {
+      let x = this;
+      if (rec.valueFunc) {
+        rec.refresh();
+      } else {
+        rec.timeout = setTimeout(function() {
+          x.del(rec.key);
+          if (timeoutCallback) {
+            timeoutCallback(key);
+          }
+        }, rec.expire);
+      }
+    }
+    this._store.set(key.toString(), rec);
+  }
+
+  del(key: (string | number | symbol)): boolean {
+    let rec = this._store.get(key.toString());
+    if (rec) {
+      clearTimeout(rec.timeout);
+      this._store.delete(key.toString());
+      return true;
+    }
+    return false;
+  }
+
+  clear(): void {
+    for (let val of this._store.values()) {
+      clearTimeout(val.timeout);
+    }
+    this._store.clear();
+  }
+
+  size(): number {
+    return this._store.size;
+  }
+
+  keys(): Array < any > {
+    let res: Array < String > = new Array < String > ();
+    for (let key of this._store.keys()) {
+      res.push(key);
+    }
+    return res;
+  }
+
+}
+
+export default Cache;
