527dd082de5298841a70edbe870dc0ebbc5cbca2
Nitin Bansal <1055173+nitinbansal@users.noreply.github.com>
2022-11-22 11:47:10 +0530

    es modules default

--- distCjs/types/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StoreValue = exports.StoreType = void 0;
-const StoreType_1 = require("./StoreType");
-exports.StoreType = StoreType_1.default;
-const StoreValue_1 = require("./StoreValue");
-exports.StoreValue = StoreValue_1.default;
-//# sourceMappingURL=index.js.map
