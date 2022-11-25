304011d23224876e65ee525d63f9ae0234d9cd5a
Nitin Bansal <1055173+nitinbansal@users.noreply.github.com>
2022-11-25 13:06:44 +0530

    module default

--- dist/index.js
+++ dist/index.js
@@ -53,5 +53,5 @@ class Cache {
         });
     }
 }
-export { Cache };
+export default Cache;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -72,4 +72,4 @@ class Cache<K, V extends any> {
 
 }
 
-export { Cache };
+export default Cache;
